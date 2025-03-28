885f6db5ce0134c8001d24e3661b58f6684f72ae
barbaradsv <babisolua@gmail.com>
2025-03-28 10:57:24 -0300

    Testando código

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,28 @@ public class Main {
         ArrayList<Conta> contas = new ArrayList<>();
 
         Cliente barbara = new Cliente("Bárbara","07789563155","Rua moraujo, 376", "barbara@gmail.com", "85998546312");
-
-
         Conta contaCorrenteBarbara = new ContaCorrente(barbara);
         Conta contaPoupancaBarbara = new ContaPoupanca(barbara);
-
         contas.add(contaCorrenteBarbara);
         contas.add(contaPoupancaBarbara);
+        contaCorrenteBarbara.depositar(1000);
 
-        Cliente Valeria = new Cliente("Valéria", "45218759545", "Rua 17", "valeria@gmail.com", "85978985463");
-
-
-        Conta contaCorrenteValeria = new ContaCorrente(Valeria);
-        Conta contaPoupancaValeria = new ContaPoupanca(Valeria);
-
+        Cliente valeria = new Cliente("Valéria", "45218759545", "Rua 17", "valeria@gmail.com", "85978985463");
+        Conta contaCorrenteValeria = new ContaCorrente(valeria);
+        Conta contaPoupancaValeria = new ContaPoupanca(valeria);
         contas.add(contaCorrenteValeria);
         contas.add(contaPoupancaValeria);
+        contaPoupancaValeria.depositar(500);
+
+        Cliente joao = new Cliente("Joao", "02597563155", "Rua XV", "joao@gmail.com", "8599999999");
+        Conta contaCorrenteJoao = new ContaCorrente(joao);
+        contas.add(contaCorrenteJoao);
 
         banco.setContas(contas);
 
         banco.listarClientes();
+        banco.listarContasPoupancas();
+        banco.listarContasCorrentes();
+        banco.saldoDeTodasContas();
     }
 }
